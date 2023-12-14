53930ccc4f29edea2bcc62d4582d281aa9bc7e64
Desarrollo Nylon <desarrollonylon@cidugotexbta.loc>
2023-12-14 14:25:43 -0500

    first commit

---
+++ Modelo/ficha.js
@@ -0,0 +1,193 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let ficha = new Schema(
+    {
+        name: {
+            type: String,
+        },
+        tipo: {
+            type: String,
+        },
+        uno: {
+            type: Number,
+        },
+        dos: {
+            type: Number,
+        },
+        tres: {
+            type: Number,
+        },
+        cuatro: {
+            type: Number,
+        },
+        cinco: {
+            type: Number,
+        },
+        seis: {
+            type: Number,
+        },
+        siete: {
+            type: Number,
+        },
+        ocho: {
+            type: Number,
+        },
+        nueve: {
+            type: Number,
+        },
+        diez: {
+            type: Number,
+        },
+        once: {
+            type: Number,
+        },
+        doce: {
+            type: Number,
+        },
+        tspand: {
+            type: Number,
+        },
+        tnylonext: {
+            type: Number,
+        },
+        tnylonint: {
+            type: Number,
+        },
+        psup: {
+            type: Number,
+        },
+        pinf: {
+            type: Number,
+        },
+        puestos: {
+            type: Number,
+        },
+        alm: {
+            type: Number,
+        },
+        velcrom: {
+            type: Number,
+        },
+        velest: {
+            type: Number,
+        },
+        velreco: {
+            type: Number,
+        },
+        tpminf: {
+            type: Number,
+        },
+        tpmsup: {
+            type: Number,
+        },
+        rpmhuso: {
+            type: Number,
+        },
+        rpmhusos: {
+            type: Number,
+        },
+        titfin: {
+            type: Number,
+        },
+        tiempo: {
+            type: Number,
+        },
+        grhora: {
+            type: Number,
+        },
+        proddia: {
+            type: Number,
+        },
+        relcogida: {
+            type: Number,
+        },
+        rendimiento: {
+            type: Number,
+        },
+        spandex: {
+            type: Number,
+        },
+        nylon: {
+            type: Number,
+        },
+        cobertura: {
+            type: String,
+        },
+        trece: {
+            type: Number,
+        },
+        catorce: {
+            type: Number,
+        },
+        modelo: {
+            type: String,
+        },
+        tipohuso: {
+            type: String,
+        },
+        tipocarre: {
+            type: String,
+        },
+        capcarre: {
+            type: Number,
+        },
+        huso: {
+            type: Number,
+        },
+        opcion: {
+            type: String,
+        },
+        tipoe: {
+            type: String,
+        },
+        tiponi: {
+            type: String,
+        },
+        proveeni: {
+            type: String,
+        },
+        proveene: {
+            type: String,
+        },
+        tipos: {
+            type: String,
+        },
+        provees: {
+            type: String,
+        },
+        obs: {
+            type: String,
+        },
+        elong: {
+            type: String,
+        },
+        encog: {
+            type: String,
+        },
+        cliente: {
+            type: String,
+        },
+        descrip: {
+            type: String,
+        },
+        nylontex: {
+            type: String,
+        },
+        nylontexi: {
+            type: String,
+        },
+        spantex: {
+            type: String,
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        },
+    },
+    {
+        collection: "ficha",
+    }
+);
+
+module.exports = mongoose.model("ficha", ficha);
--- Rutas/rutas.js
+++ Rutas/rutas.js
@@ -23,6 +23,16 @@ ruta.get('/recubrir', (req, res) => {
   });
 });
 
+ruta.get('/ficha', (req, res) => {
+  ficha.find((error, data, next) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
 const bcrypt = require("bcrypt");
 
 ruta.post('/regrecubrir', async (req, res, next) => {
@@ -84,6 +94,81 @@ ruta.post('/regrecubrir', async (req, res, next) => {
   });
 });
 
+ruta.post('/regficha', async (req, res, next) => {
+
+  const ingreso = new ficha({
+    name: req.body.name,
+    tipo: req.body.tipo,
+    uno: req.body.uno,
+    dos: req.body.dos,
+    tres: req.body.tres,
+    cuatro: req.body.cuatro,
+    cinco: req.body.cinco,
+    seis: req.body.seis,
+    siete: req.body.siete,
+    ocho: req.body.ocho,
+    nueve: req.body.nueve,
+    diez: req.body.diez,
+    once: req.body.once,
+    doce: req.body.doce,
+    tspand: req.body.tspand,
+    tnylonext: req.body.tnylonext,
+    tnylonint: req.body.tnylonint,
+    psup: req.body.psup,
+    pinf: req.body.pinf,
+    puestos: req.body.puestos,
+    alm: req.body.alm,
+    velcrom: req.body.velcrom,
+    velest: req.body.velest,
+    velreco: req.body.velreco,
+    tpminf: req.body.tpminf,
+    tpmsup: req.body.tpmsup,
+    rpmhuso: req.body.rpmhuso,
+    rpmhusos: req.body.rpmhusos,
+    titfin: req.body.titfin,
+    tiempo: req.body.tiempo,
+    grhora: req.body.grhora,
+    proddia: req.body.proddia,
+    relcogida: req.body.relcogida,
+    rendimiento: req.body.rendimiento,
+    spandex: req.body.spandex,
+    nylon: req.body.nylon,
+    cobertura: req.body.cobertura,
+    trece: req.body.trece,
+    catorce: req.body.catorce,
+    modelo: req.body.modelo,
+    tipohuso: req.body.tipohuso,
+    tipocarre: req.body.tipocarre,
+    capcarre: req.body.capcarre,
+    huso: req.body.huso,
+    opcion: req.body.opcion,
+    tipoe: req.body.tipoe,
+    tiponi: req.body.tiponi,
+    proveeni: req.body.proveeni,
+    proveene: req.body.proveene,
+    tipos: req.body.tipos,
+    provees: req.body.provees,
+    obs: req.body.obs,
+    elong: req.body.elong,
+    encog: req.body.encog,
+    cliente: req.body.cliente,
+    descrip: req.body.descrip,
+    nylontex: req.body.nylontex,
+    nylontexi: req.body.nylontexi,
+    spantex: req.body.spantex,
+  })
+
+  ficha.create(ingreso, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      console.log(data);
+      res.json(data);
+    }
+  });
+});
+
+
 ruta.get("/edit-student/:id", (req, res) => {
   recubrir.findById(req.params.id, (error, data, next) => {
     if (error) {
@@ -112,8 +197,39 @@ ruta.put("/update-student/:id", (req, res, next) => {
   );
 });
 
+ruta.put("/updateficha/:id", (req, res, next) => {
+  ficha.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        console.log(error);
+        return next(error);
+      } else {
+        res.json(data);
+        console.log("Student successfully updated!");
+      }
+    }
+  );
+});
+
+
 ruta.delete("/delrecubrir/:id", (req, res, next) => {
   recubrir.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+});
+
+ruta.delete("/delficha/:id", (req, res, next) => {
+  ficha.findByIdAndRemove(req.params.id, (error, data) => {
     if (error) {
       return next(error);
     } else {
