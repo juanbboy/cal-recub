d382592d2e3397f8f74911a7f921e58afa237104
Desarrollo Nylon <desarrollonylon@cidugotexbta.loc>
2023-12-14 16:32:45 -0500

    first commit

--- Modelo/ficha.js
+++ Modelo/ficha.js
@@ -182,7 +182,7 @@ let ficha = new Schema(
         },
         date: {
             type: String,
-            default: new Date(new Date().utc(-5))
+            default: new Date(new Date().getTime(-5))
         },
     },
     {
